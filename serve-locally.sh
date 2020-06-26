770daa9f8609425aefb19755e8c45950ab77c0d1
GeraldLoeffler <gerald.loeffler@googlemail.com>
2020-06-26 10:32:29 +0200

    tentatively complete basis

--- serve-locally.sh
+++ serve-locally.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Run GitHub Pages container for this repo
-# serving site at http://0.0.0.0:4000
+# serving site at http://127.0.0.1:4000
+
+docker pull integrational/gh-pages
 
 docker run --name gh-pages --rm -it       \
   -p 4000:4000                            \
